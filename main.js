94c5ee825ca1d401a30e16e7a996237f28441f0b
Shepon Biswas <sheponbiswas01@gmail.com>
2023-03-03 02:37:50 +0600

    Fetch API and display all the data with Name and Images

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("Connected");
+// fetch api
+
+const loadAIData = () =>{
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then(res => res.json())
+    .then(data => displayAIData(data.data.tools));
+}
+
+// display AI function
+const displayAIData = AIDatas =>{
+    // console.log(AIDatas);
+    const AIDataContainer = document.getElementById('AI-data-container');
+    AIDatas.forEach(AIData =>{
+        console.log(AIData);
+        const AIDataDiv = document.createElement('div');
+        AIDataDiv.classList.add('col');
+
+        AIDataDiv.innerHTML = `
+        <div class="card h-100 p-3">
+                <img src="${AIData.image}" class="card-img-top " alt="..." />
+                <div class="card-body">
+                    
+                    <h5 class="card-title">${AIData.name}</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                    </p>
+                </div>
+              </div>
+        
+        `;
+        AIDataContainer.appendChild(AIDataDiv);
+    })
+}
+
+
+
+loadAIData();
